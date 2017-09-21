b452ebfd0526c73feed8166b9585d650f11f9e25
alvarvelazquezdeleonlavarrios <31053907+alvarvelazquezdeleonlavarrios@users.noreply.github.com>
2017-09-20 19:40:52 -0500

    Reporte Práctica 6

---
+++ hola.c
@@ -0,0 +1,6 @@
+#include<stdio.h>
+
+int main(){
+    printf("Konnichiwa\n");
+    return 0;
+}
---
+++ resta.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+int a,b,c;
+
+main(){
+    printf("Ingrese el primer número: ");
+    scanf("%d",&a);
+
+    printf("Ingrese el segundo número: ");
+    scanf("%d",&b);
+
+    c=a-b;
+    printf("El resultado es: %d",c);
+}
